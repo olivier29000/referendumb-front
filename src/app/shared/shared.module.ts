6615c953ea9e5588ae3b02cddc3754b5935dbaf5
olivier29000 <lasbleis.olivier@yahoo.fr>
2025-01-25 19:35:44 +0100

    footer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,35 +1,36 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { NgbDatepickerModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NgxDropzoneModule } from 'ngx-dropzone';
-import { CarouselModule } from 'ngx-owl-carousel-o';
-import { ChartistModule } from 'ng-chartist';
-import { NgApexchartsModule } from 'ng-apexcharts';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { GoogleMapsModule } from '@angular/google-maps';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+import { NgbDatepickerModule, NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { NgxDropzoneModule } from "ngx-dropzone";
+import { CarouselModule } from "ngx-owl-carousel-o";
+import { ChartistModule } from "ng-chartist";
+import { NgApexchartsModule } from "ng-apexcharts";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { GoogleMapsModule } from "@angular/google-maps";
 
-import { MenuComponent } from './components/menu/menu.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { LayoutComponent } from './components/layouts/layout/layout.component';
-import { FeatherIconsComponent } from './components/feather-icons/feather-icons.component';
-import { LogoComponent } from './components/menu/widgets/logo/logo.component';
-import { ProfileComponent } from './components/menu/widgets/profile/profile.component';
-import { HelpComponent } from './components/menu/widgets/help/help.component';
-import { SearchComponent } from './components/header/widgets/search/search.component';
-import { FullScreenComponent } from './components/header/widgets/full-screen/full-screen.component';
-import { AttachmentsComponent } from './components/header/widgets/attachments/attachments.component';
-import { NotificationComponent } from './components/header/widgets/notification/notification.component';
-import { MessageComponent } from './components/header/widgets/message/message.component';
-import { UserProfileComponent } from './components/header/widgets/user-profile/user-profile.component';
-import { BreadcrumbComponent } from './components/ui/breadcrumb/breadcrumb.component';
-import { FullLayoutComponent } from './components/layouts/full-layout/full-layout.component';
-import { CustomizerComponent } from './components/customizer/customizer.component';
-import { TapToTopComponent } from './components/tap-to-top/tap-to-top.component';
-import { LoaderComponent } from './components/loader/loader.component';
-import { ToastrModule } from 'ngx-toastr';
-import { OutsideDirective } from './directive/outside.directive';
+import { MenuComponent } from "./components/menu/menu.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { LayoutComponent } from "./components/layouts/layout/layout.component";
+import { FeatherIconsComponent } from "./components/feather-icons/feather-icons.component";
+import { LogoComponent } from "./components/menu/widgets/logo/logo.component";
+import { ProfileComponent } from "./components/menu/widgets/profile/profile.component";
+import { HelpComponent } from "./components/menu/widgets/help/help.component";
+import { SearchComponent } from "./components/header/widgets/search/search.component";
+import { FullScreenComponent } from "./components/header/widgets/full-screen/full-screen.component";
+import { AttachmentsComponent } from "./components/header/widgets/attachments/attachments.component";
+import { NotificationComponent } from "./components/header/widgets/notification/notification.component";
+import { MessageComponent } from "./components/header/widgets/message/message.component";
+import { UserProfileComponent } from "./components/header/widgets/user-profile/user-profile.component";
+import { BreadcrumbComponent } from "./components/ui/breadcrumb/breadcrumb.component";
+import { FullLayoutComponent } from "./components/layouts/full-layout/full-layout.component";
+import { CustomizerComponent } from "./components/customizer/customizer.component";
+import { TapToTopComponent } from "./components/tap-to-top/tap-to-top.component";
+import { LoaderComponent } from "./components/loader/loader.component";
+import { ToastrModule } from "ngx-toastr";
+import { OutsideDirective } from "./directive/outside.directive";
+import { FooterDumb } from "./dumbs/footer.dumb";
 
 @NgModule({
   declarations: [
@@ -52,7 +53,8 @@ import { OutsideDirective } from './directive/outside.directive';
     CustomizerComponent,
     TapToTopComponent,
     LoaderComponent,
-    OutsideDirective
+    OutsideDirective,
+    FooterDumb,
   ],
   imports: [
     CommonModule,
@@ -81,7 +83,8 @@ import { OutsideDirective } from './directive/outside.directive';
     GoogleMapsModule,
     TapToTopComponent,
     LoaderComponent,
-    ToastrModule
-  ]
+    ToastrModule,
+    FooterDumb,
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}
